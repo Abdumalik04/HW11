300f463702ecaec3fa18744d59170a654e340222
User <asanbekovmalik2@gmail.com>
2023-02-01 18:39:57 +0600

    Ozumo

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+
+    int year;
+     String brand;
+     String color;
+
+    public static void drive(){
+        System.out.println("mashina jurdu");
+    }
+    static void tormoz(){
+        System.out.println("mashina toktodu");
+    }
+}
